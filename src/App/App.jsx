d7789975f02e0dea8c25773e70916cfb080a46ef
Divyx09 <divyaattarde94@gmail.com>
2025-03-22 13:05:55 +0530

    1

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,33 @@
-import AppRouter from "./router/AppRouter";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "./layouts/RootLayout";
+import HomePage from "../pages/Home/HomePage";
+import NotFoundPage from "../pages/NotFound/NotFoundPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Navbar />
+        <RootLayout />
+        <Footer />
+      </>
+    ),
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  return <AppRouter />;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -4,9 +4,7 @@ import Navbar from "../../components/Navbar";
 const HomePage = () => {
   return (
     <div>
-      <Navbar />
       <h1>Home Page</h1>
-      <Footer/>
     </div>
   );
 };
