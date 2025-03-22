964689b2f141e267704f0d93ed16645efb3e0f04
Divyx09 <divyaattarde94@gmail.com>
2025-03-22 12:42:17 +0530

    1

--- src/App/layouts/RootLayout.jsx
+++ src/App/layouts/RootLayout.jsx
@@ -1,8 +1,8 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return (
-    <div className="root-layout">
+    <div className='root-layout'>
       <main>
         <Outlet />
       </main>
