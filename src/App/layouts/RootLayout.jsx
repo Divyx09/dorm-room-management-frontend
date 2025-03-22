95f691e58ffe362f1ba23acfdda7ed9ea239806f
Divyx09 <divyaattarde94@gmail.com>
2025-03-22 12:41:26 +0530

    1

--- src/App/layouts/RootLayout.jsx
+++ src/App/layouts/RootLayout.jsx
@@ -3,11 +3,9 @@ import { Outlet } from 'react-router-dom';
 const RootLayout = () => {
   return (
     <div className="root-layout">
-      {/* Add your header, navigation, etc. here */}
       <main>
         <Outlet />
       </main>
-      {/* Add your footer here */}
     </div>
   );
 };
